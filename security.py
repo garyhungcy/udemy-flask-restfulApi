1492998c1507f60e3cda7c4983189488c146bcb0
Elle Wong <cheetahxspeed@gmail.com>
2018-05-29 21:29:01 +0800

    Test if code from teacher is working

--- security.py
+++ security.py
@@ -1,11 +1,11 @@
-from models.user import UserModel
 from werkzeug.security import safe_str_cmp
-
+from models.user import UserModel
 
 def authenticate(username, password):
     user = UserModel.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
-def identity(playload):
-    return UserModel.find_by_id(playload["identity"])
+def identity(payload):
+    user_id = payload['identity']
+    return UserModel.find_by_id(user_id)
