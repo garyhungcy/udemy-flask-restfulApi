4611a6f030836ca783fb7d91d6565c30b48595a2
Elle Wong <cheetahxspeed@gmail.com>
2018-05-29 20:53:22 +0800

    minor updates

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "Elle"
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
